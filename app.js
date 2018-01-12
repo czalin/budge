f90a0a5f24190ed8f5d8c4cdec5440a8a7c9fa13
Calin Scott <czalin@gmail.com>
2018-01-12 00:07:16 -0600

    RESTful SPA

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// Database
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('budge-node:scowenandbott@ds046267.mlab.com:46267/budge');
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -14,12 +19,19 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Database access for routes
+app.use(function(req,res,next) {
+	req.db = db;
+	next();
+});
 
 app.use('/', index);
 app.use('/users', users);
