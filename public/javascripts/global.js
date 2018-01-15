3e3a1508eb12535edfb53056e79546d9e5b8e3af
Calin Scott <czalin@gmail.com>
2018-01-15 13:53:11 -0600

    Get BudgetRanges with UserId

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,3 +1,13 @@
+// GLOBALS ==============================================================
+var budgeUser = {
+	userId: '1',
+	budgetRanges: {}
+};
+
 // DOM Ready ============================================================
 $(document).ready(function() {
+	// Get budget ranges for current user
+	$.getJSON('/budgetRanges/' + budgeUser.userId, function(data) {
+		budgeUser.budgetRanges = data;
+	});
 });
