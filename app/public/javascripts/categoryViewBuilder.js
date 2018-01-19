7dc0e093d2386fdb804905c911af35447aa916d7
Calin Scott <czalin@gmail.com>
2018-01-19 10:02:18 -0600

    Finished cleaning up the code and fixed scrolling through expenses.

--- app/public/javascripts/categoryViewBuilder.js
+++ app/public/javascripts/categoryViewBuilder.js
@@ -16,8 +16,6 @@ function buildCategoryView(currentCategoryId) {
 		}
 	});
 
-	var catCircle;
-
 	// Update the background image based on the current category
 	$('body').css('background-image', 'url(' + currentCategory.imageURL + ')');
 
@@ -38,7 +36,8 @@ function buildCategoryView(currentCategoryId) {
 	// Create the base table for the view
 	var categoryViewTable = document.createElement('table');
 	categoryViewTable.insertRow(0).id = 'categoryViewRow';
-	categoryViewTable.style.margin = 'auto';
+	//categoryViewTable.style.margin = 'auto';
+	categoryViewTable.style.width = '100%';
 	categoryViewTable.style.height = '100%';
 	$('#wrapper').append(categoryViewTable);
 
@@ -54,7 +53,9 @@ function buildCategoryView(currentCategoryId) {
 	  portrait mode) any time expenses are changed
 	*******************************************************/
 
+	var catCircle;
 	function updateVisualizerCell() {
+		// Render the visualizer cell
 		visualizerCell.innerHTML = 
 			'<table style="height: 100%; margin: auto">' +
 				'<tr id="remainingRow" style="height: 0px">' +
@@ -84,15 +85,42 @@ function buildCategoryView(currentCategoryId) {
 				'</tr>' +
 			'</table>';
 
-		catCircle = new BudgeCircle(currentCategory, $(window).width()/7);
+		// Create the category circle based on screen orientation
+		if($(window).width() > $(window).height()) {
+			catCircle = new BudgeCircle(currentCategory, $(window).width()/7);
+		} else {
+			catCircle = new BudgeCircle(currentCategory, $(window).height()/4);
+		}
+
 		// Add the circle container to the screen
 		$('#circleCell').append(catCircle.container);
+
+		// Draw the circle
+		catCircle.draw();
+
+		/************************* SWITCH CATEGORIES **********************************
+		- Handle switching categories when clicking on the circle
+		*******************************************************************************/
+
+		$('#circleCell').click(function() {
+			var nextCatIndex = budgeUser.currentRangeCategories.indexOf(currentCategory) + 1;
+			if(nextCatIndex >= budgeUser.currentRangeCategories.length) {
+				nextCatIndex = 0;
+			}
+			buildCategoryView(budgeUser.currentRangeCategories[nextCatIndex]._id);
+		});
 	}
+	buildCategoryView.updateVisualizerCell = updateVisualizerCell;
 
 	// Set up the expense table
 	var expenseTableCell = document.createElement('td');
 	expenseTableCell.id = 'expenseTableCell';
 	expenseTableCell.style.width = 'calc(100vw * 2 / 3)';
+	$('#categoryViewRow').append(expenseTableCell);
+
+	if($(window).width() < $(window).height()) {
+		$('#expenseTableCell').hide();
+	}
 	updateExpenseTableCell();
 
 	/*********** FUNCTION updateExpenseTableCell ***********
@@ -105,19 +133,27 @@ function buildCategoryView(currentCategoryId) {
 		var expenseRows = '';
 		$.each(budgeUser.currentRangeExpenses, function(index, expense) {
 			if(expense.categoryId === currentCategory._id) {
-				expenseRows += '<tr class="expenseRow" data-id=' + expense._id + '><td>' + expense.date + '</td><td>' + expense.description + '</td><td>$' + parseFloat(expense.amount).toFixed(2) + '</td></tr>';
+				expenseRows += '<tr class="expenseRow" data-id=' + expense._id + '>' + 
+							       '<td style="width: 20%">' + expense.date + '</td>' + 
+							       '<td style="width: 60%">' + expense.description + '</td>' + 
+							       '<td style="width: 20%">$' + parseFloat(expense.amount).toFixed(2) + '</td>' + 
+							    '</tr>';
 			}
 		});
 
+		// Render the expense table cell
 		expenseTableCell.innerHTML =
 			'<div style="width: calc(100% - 30px); background-color: rgba(0,0,0,0.5); margin: 10px; padding: 5px; border-radius: 10px;">' +
-				'<div style="max-height: 60vh; overflow-y: scroll;">' +
-					'<table style="width: 100%; font-family: geosans; color: white; text-align: center;">' +
-						'<tr>' +
-							'<th>Date</th>' +
-							'<th>Description</th>' +
-							'<th>Amount</th>' +
-						'</tr>' +
+				'<table style="width: calc(100% - 5px); font-family: geosans; color: white; text-align: center; margin-right: 5px;">' +
+					'<tr>' +
+						'<th style="width: 20%;">Date</th>' +
+						'<th style="width: 60%;">Description</th>' +
+						'<th style="width: 20%;">Amount</th>' +
+					'</tr>' +
+				'</table>' +
+				'<hr style="width: 90%; height: 2px; border: none; background-color: white;">' +
+				'<div style="max-height: 50vh; overflow-y: scroll;">' +
+					'<table style="width:100%; font-family: geosans; color: white; text-align: center;">' +
 						expenseRows +
 					'</table>' +
 				'</div>' +
@@ -125,73 +161,11 @@ function buildCategoryView(currentCategoryId) {
 				'<table style="width: 100%; text-align: center;">' +
 					'<tr>' +
 						'<td style="border-radius: 10px;"><h2 id="addExpense" style="font-size: 20pt; line-height: normal">Add Expense</h2></td>' +
+						'<td style="width: 10px"></td>' +
 						'<td style="border-radius: 10px;"><h2 id="addMultiple" style="font-size: 20pt; line-height: normal">Add Multiple</h2></td>' +
 					'</tr>' +
 				'</table>' +
 			'</div>';
-	}
-
-	
-	function renderLandscape() {
-		$('#wrapper').html(
-			'<table style="height: 100%; margin: auto;">' +
-				'<tr>' +
-					'<td>' +
-						'<table style="margin: auto;">' +
-							'<tr id="remainingRow">' +
-								'<td>' +
-									'<table style="width: 100%">' +
-										'<tr>' +
-											'<td><h2>Remaining</h2></td>' +
-											'<td><h3> | </h3></td>' +
-											'<td><h3>$' + currentCategory.remainder.toFixed(2).toString() + '</h3></td>' +
-										'</tr>' +
-									'</table>' +
-								'</td>' +
-							'</tr>' +
-							'<tr>' +
-								'<td id="circleCell"></td>' +
-							'</tr>' +
-							'<tr id="budgetRow">' +
-								'<td>' +
-									'<table style="width: 100%">' +
-										'<tr>' +
-											'<td><h2>Budget</h2></td>' +
-											'<td><h3> | </h3></td>' +
-											'<td><h3>$' + currentCategory.amount.toFixed(2).toString() + '</h3></td>' +
-										'</tr>' +
-									'</table>' +
-								'</td>' +
-							'</tr>' +
-						'</table>' +
-					'</td>' +
-					'<td style="width: calc(100vw * 2 / 3);">' +
-						'<div style="width: calc(100% - 30px); background-color: rgba(0,0,0,0.5); margin: 10px; padding: 5px; border-radius: 10px;">' +
-							'<div style="max-height: 60vh; overflow-y: scroll;">' +
-								'<table style="width: 100%; font-family: geosans; color: white; text-align: center;">' +
-									'<tr>' +
-										'<th>Date</th>' +
-										'<th>Description</th>' +
-										'<th>Amount</th>' +
-									'</tr>' +
-									expenseRows +
-								'</table>' +
-							'</div>' +
-							'<hr style="width: 90%; height: 2px; border: none; background-color: white;">' +
-							'<table style="width: 100%; text-align: center;">' +
-								'<tr>' +
-									'<td style="border-radius: 10px;"><h2 id="addExpense" style="font-size: 20pt; line-height: normal">Add Expense</h2></td>' +
-									'<td style="border-radius: 10px;"><h2 id="addMultiple" style="font-size: 20pt; line-height: normal">Add Multiple</h2></td>' +
-								'</tr>' +
-							'</table>' +
-						'</div>' +
-					'</td>' +
-				'</tr>' +
-			'</table>'
-		);
-
-		// Add the circle container to the screen
-		$('#circleCell').append(catCircle.container);
 
 		/****************************** ADD EXPENSE ********************************
 		// Handle expense adding by building a new expense view with default values
@@ -217,7 +191,6 @@ function buildCategoryView(currentCategoryId) {
 			$(this).parent().css('background-color', 'transparent');
 			$(this).parent().css('transition', 'all 0.25s');
 		}
-
 
 		$('#addExpense').click(function() {
 			var today = new Date();
@@ -253,91 +226,18 @@ function buildCategoryView(currentCategoryId) {
 				}
 			});
 		});
-
-		/************************* SWITCH CATEGORIES **********************************
-		- Handle switching categories when clicking on the circle
-		*******************************************************************************/
-
-		$('#circleCell').click(function() {
-			var nextCatIndex = budgeUser.currentRangeCategories.indexOf(currentCategory) + 1;
-			if(nextCatIndex >= budgeUser.currentRangeCategories.length) {
-				nextCatIndex = 0;
-			}
-			buildCategoryView(budgeUser.currentRangeCategories[nextCatIndex]._id);
-		});
 	}
+	buildCategoryView.updateExpenseTableCell = updateExpenseTableCell;
 
-	function renderPortrait() {
-		$('#wrapper').html(
-			'<table style="height: 100%; margin: auto;">' +
-				'<tr>' +
-					'<td id="visualizerCell">' +
-						'<table style="height: 100%; margin: auto">' +
-							'<tr id="remainingRow" style="height: 0px">' +
-								'<td>' +
-									'<table style="width: 100%">' +
-										'<tr style="height: 100%">' +
-											'<td><h2>Remaining</h2></td>' +
-											'<td><h3> | </h3></td>' +
-											'<td><h3>$' + currentCategory.remainder.toFixed(2).toString() + '</h3></td>' +
-										'</tr>' +
-									'</table>' +
-								'</td>' +
-							'</tr>' +
-							'<tr>' +
-								'<td id="circleCell"></td>' +
-							'</tr>' +
-							'<tr id="budgetRow" style="height: 0px">' +
-								'<td>' +
-									'<table style="width: 100%">' +
-										'<tr>' +
-											'<td><h2>Budget</h2></td>' +
-											'<td><h3> | </h3></td>' +
-											'<td><h3>$' + currentCategory.amount.toFixed(2).toString() + '</h3></td>' +
-										'</tr>' +
-									'</table>' +
-								'</td>' +
-							'</tr>' +
-						'</table>' +
-					'</td>' +
-				'</tr>' +
-			'</table>'
-		);
-
-		// Add the circle container to the screen
-		$('#circleCell').append(catCircle.container);
-
-		/************************* SWITCH CATEGORIES **********************************
-		- Handle switching categories when clicking on the circle
-		*******************************************************************************/
-		
-		$('#circleCell').click(function() {
-			var nextCatIndex = budgeUser.currentRangeCategories.indexOf(currentCategory) + 1;
-			if(nextCatIndex >= budgeUser.currentRangeCategories.length) {
-				nextCatIndex = 0;
-			}
-			buildCategoryView(budgeUser.currentRangeCategories[nextCatIndex]._id);
-		});
-	}
-
-	// Draw the circle
-	catCircle.draw();
-
-	// Handle window resize events
 	$(window).resize(function() {
 		if($(window).width() > $(window).height()) {
+			$('#expenseTableCell').show();
 			catCircle.resize($(window).width()/7);
-			if(!isLandscape) {
-				renderLandscape();
-				isLandscape = true;
-			}
+			catCircle.draw();
 		} else {
+			$('#expenseTableCell').hide();
 			catCircle.resize($(window).height()/4);
-			if(isLandscape) {
-				renderPortrait();
-				isLandscape = false;
-			}
+			catCircle.draw();
 		}
-		catCircle.draw();
 	});
 }
